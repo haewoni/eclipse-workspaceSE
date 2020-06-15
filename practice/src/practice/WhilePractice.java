7fd0b66a836279ecc2c938908c68b5852f8edcb9
haewoni <suzyhw96@gmail.com>
2020-06-15 18:55:13 +0900

    .

--- practice/src/practice/WhilePractice.java
+++ practice/src/practice/WhilePractice.java
@@ -1,73 +1,27 @@
 package practice;
 
 public class WhilePractice {
-
+	public static void main(String[] args) {
 
 		/*
-		 * 1~10사이의 정수출력
+		 * ★[i=0,j=0]★[i=0,j=1]★[i=0,j=2]★[i=0,j=3]★[i=0,j=4]
+		 * ★[i=1,j=0]★[i=1,j=1]★[i=1,j=2]★[i=1,j=3]★[i=1,j=4]
+		 * ★[i=2,j=0]★[i=2,j=1]★[i=2,j=2]★[i=2,j=3]★[i=2,j=4]
+		 * ★[i=3,j=0]★[i=3,j=1]★[i=3,j=2]★[i=3,j=3]★[i=3,j=4]
+		 * ★[i=4,j=0]★[i=4,j=1]★[i=4,j=2]★[i=4,j=3]★[i=4,j=4]
 		 */
 		int i=0;
-		while (i < 10) {
-			int su = i+1;
-			System.out.print(su+",");
+		while (i<5) {
+			int j =0;
+			while (j <5) {
+				System.out.printf("%s[i=%d,j=%d]","★",i,j);
+				j++;
+			}
+			System.out.println();
 			i++;
 		}
 		System.out.println();
-		/*
-		 * 1~10사이의 정수중 홀수만출력
-		 */
-		int j=0;
-		while(j<10) {
-			int su=j+1;
-			if(su%2==1) {
-				System.out.print(su+",");
-			}	
-			j++;
-		}
-		System.out.println();
-		/*
-		 * 1~10사이의 정수중 4의배수만출력
-		 */
-		int k=0;
-		while(k<10) {
-			int su=k+1;
-			if(su%4==0) {
-				System.out.print(su+",");
-			}
-			k++;
-		}
-		System.out.println();
-		/*
-		 * 1~100 사이의 정수중 3과4의배수만출력
-		 */
-		int l=1;
-		while(l <= 100) {
-			if(l%3==0 && l%4==0) {
-				System.out.print(l+",");
-			}	
-			l++;
-		}
-		System.out.println();
-		/* Quiz:
-		 * 1 ~ 2020 년사이의 년도중 윤년만 출력
-		 */
-		
-		/*
-		 * 1~100사이의 정수합
-		 * 1~100사이의 정수중 홀수합(Quiz)    
-		 * 1~100사이의 정수중 짝수합(Quiz)    
-		 */
-	
-		
-		
+		i++;
+
 	}
-
 }
-
-
-
-
-
-
-
-
