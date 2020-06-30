f62af0055f8773fa998a8de696552bfb65607e0b
haewoni <suzyhw96@gmail.com>
2020-06-30 18:01:30 +0900

    .

---
+++ 17.예외[Exception]/src/InsufficientBalanceException.java
@@ -0,0 +1,10 @@
+
+public class InsufficientBalanceException extends Exception {
+	public InsufficientBalanceException() {
+
+	}
+	public InsufficientBalanceException(String msg) {
+		super(msg);
+	}
+	
+}
